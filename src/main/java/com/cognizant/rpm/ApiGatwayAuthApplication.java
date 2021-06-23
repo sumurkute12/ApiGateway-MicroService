765dbfc8880fa02aef350beb0e30267bdd787223
Swapnil <murkute.swapnil12@gmail.com>
2021-06-23 09:02:55 +0530

    Swapnil thirdcommit

--- src/main/java/com/cognizant/rpm/ApiGatwayAuthApplication.java
+++ src/main/java/com/cognizant/rpm/ApiGatwayAuthApplication.java
@@ -2,6 +2,7 @@ package com.cognizant.rpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @SpringBootApplication
 @EnableZuulProxy
 @EnableWebSecurity
+@EnableDiscoveryClient
 
 public class ApiGatwayAuthApplication {
 
